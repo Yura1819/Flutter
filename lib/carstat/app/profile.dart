1f521972daacada57f3a1d3ee250455a16f015d1
Yura <yuriy13052004@gmail.com>
2024-11-29 10:03:04 +0200

    lab2

---
+++ lib/carstat/app/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart'; 
+import 'package:my_project/carstat/profile/body.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: const ProfileBody(),
+    );
+  }
+}
